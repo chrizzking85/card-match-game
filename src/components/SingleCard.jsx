133f97e929cd5eb95fd618f2417c3e2bf538f68e
Felix <felix.sinne@googlemail.com>
2022-01-12 12:46:49 +0100

    import image

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,10 +1,11 @@
 import './SingleCard.css'
+import cover from '/src/img/cover.png'
 
 const SingleCard = ({card, handleChoice, flipped, disabled}) => (
     <div className="card">
       <div className={flipped ? "flipped":""}>
           <img className="front" src={card.src}></img>
-          <img className="back" src="src/img/cover.png" onClick={() => !disabled && handleChoice(card)}></img>
+          <img className="back" src={cover} onClick={() => !disabled && handleChoice(card)}></img>
       </div>
     </div>
 )
