502cef60171d183f2390d4839c7da10f91b98d08
Felix <felix.sinne@googlemail.com>
2022-01-12 13:08:07 +0100

    import image

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,5 +1,11 @@
 import './SingleCard.css'
 import cover from '/src/img/cover.png'
+import pic1 from "/src/img/helmet-1.png"
+import pic2 from "/src/img/potion-1.png"
+import pic3 from "/src/img/ring-1.png"
+import pic4 from "/src/img/scroll-1.png"
+import pic5 from "/src/img/shield-1.png"
+import pic6 from "/src/img/sword-1.png"
 
 const SingleCard = ({card, handleChoice, flipped, disabled}) => (
     <div className="card">
